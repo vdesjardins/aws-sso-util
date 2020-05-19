dcf66937d25420b724583df274658f5292a81e86
Ben Kehoe <ben@kehoe.io>
2020-05-19 09:13:21 -0400

    Option to disable browser dispatch

--- aws_sso_credential_process/aws_sso_credential_process.py
+++ aws_sso_credential_process/aws_sso_credential_process.py
@@ -40,7 +40,7 @@ from botocore.exceptions import ClientError
 from .utils import SSOTokenFetcher
 from .credentials import SSOCredentialFetcher
 
-__version__ = '0.2.5'
+__version__ = '0.3.0'
 
 class InvalidSSOConfigError(Exception):
     pass
@@ -278,7 +278,8 @@ class OpenBrowserHandler(object):
 
         print_tty(message)
 
-        if self._open_browser:
+        disable_browser = os.environ.get('AWS_SSO_DISABLE_BROWSER', '').lower() in ['1', 'true']
+        if self._open_browser and not disable_browser:
             try:
                 return self._open_browser(verificationUriComplete)
             except InteractiveAuthDisabledError:
